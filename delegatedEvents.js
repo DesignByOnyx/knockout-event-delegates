5292a553c5e0799d487a8ac8e75f85f4f36a4afd
Ryan Wheale <ryan.wheale@gmail.com>
2014-09-22 19:44:05 -0600

    Create delegatedEvents.js

---
+++ delegatedEvents.js
@@ -0,0 +1,49 @@
+define([
+    "knockout",
+    "jquery",
+    "underscore"
+], function (ko, $, _) {
+    ko.bindingHandlers.delegatedEvents = {
+        init: function(element, valueAccessor, allBindings, viewModel) {
+            var disposeQueue = {},
+                $element = $(element),
+                events = ko.unwrap( valueAccessor() ),
+                eventsArray = "blur focus focusin focusout load resize scroll unload click dblclick mousedown mouseup mousemove mouseover mouseout mouseenter mouseleave change select submit keydown keypress keyup error contextmenu".split(" "),
+                eventRegex = new RegExp("(.*) ?(" + eventsArray.join("|") + ")$"),
+                matchedBinding, eventNamespace;
+            
+            _.each(events, function(handler, eventKey) {
+                matchedBinding = eventKey.match( eventRegex );
+                if( matchedBinding ) {
+                    //console.log( "Binding", matchedBinding[2], "event delegate for '", (matchedBinding[1] ? matchedBinding[1] : "ALL"), "' elements");
+                    
+                    if( typeof handler === "string" ) {
+                        handler = viewModel[ handler ];
+                    }
+                    
+                    if( typeof handler !== "function" ) { return; }
+                    
+                    var handlerWrap = function(ev) {
+                        var returnValue = handler.call(viewModel, ko.contextFor(this).$data, ev);
+                        if( returnValue !== true ) {
+                            ev.preventDefault();
+                        }
+                    };
+                    
+                    eventNamespace = matchedBinding[2] + "." + matchedBinding[0].replace(/[^\w]/g, "");
+                    disposeQueue[ eventNamespace ] = handlerWrap;
+                    $element.on(eventNamespace, matchedBinding[1], handlerWrap);
+                }
+            });
+            
+            ko.utils.domNodeDisposal.addDisposeCallback(element, function() {
+                // unbind any event delegates
+                ko.utils.objectForEach(disposeQueue, function(eventNamespace, handler) {
+                    //console.log("Unbinding event: ", eventNamespace);
+                    $element.off(eventNamespace, handler);
+                    disposeQueue[ eventNamespace ] = null;
+                });
+            });
+        }
+    };
+});
